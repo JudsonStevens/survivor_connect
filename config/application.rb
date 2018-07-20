17ad65afa622c1a1b091505fa2d0079e2ad867cb
Judson Stevens <Judson.Stevens@gmail.com>
2018-07-20 09:10:17 -0600

    AWS fixes

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,12 @@ Bundler.require(*Rails.groups)
 
 module SurvivorConnect
   class Application < Rails::Application
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get]
+      end
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
@@ -38,11 +44,5 @@ module SurvivorConnect
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', headers: :any, methods: [:get]
-      end
-    end
   end
 end
