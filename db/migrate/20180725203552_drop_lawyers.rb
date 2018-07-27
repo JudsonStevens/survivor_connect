cf4e7d4e9e1831b78132fe0869a1d25bbce69301
Judson Stevens <Judson.Stevens@gmail.com>
2018-07-27 10:59:26 -0600

    Fixing eb errors

--- db/migrate/20180725203552_drop_lawyers.rb
+++
@@ -1,5 +0,0 @@
-class DropLawyers < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :lawyers
-  end
-end
--- db/migrate/20180725203731_change_user_id_column_on_user_license_areas.rb
+++ db/migrate/20180725203731_change_user_id_column_on_user_license_areas.rb
@@ -1,6 +1,5 @@
 class ChangeUserIdColumnOnUserLicenseAreas < ActiveRecord::Migration[5.2]
   def change
-    remove_column :user_license_areas, :User_id, :bigint
     add_column :user_license_areas, :user_id, :bigint
   end
 end
