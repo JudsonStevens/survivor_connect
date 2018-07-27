2a6777e0891f3c27a9928162a1b2926ef0e2cba8
Judson Stevens <Judson.Stevens@gmail.com>
2018-07-27 10:55:14 -0600

    Fixing eb errors

--- db/migrate/20180708210827_create_lawyer_license_areas.rb
+++ db/migrate/20180708210827_create_lawyer_license_areas.rb
@@ -1,7 +1,6 @@
 class CreateLawyerLicenseAreas < ActiveRecord::Migration[5.2]
   def change
     create_table :lawyer_license_areas do |t|
-      t.references :lawyer, foreign_key: true
       t.references :license_area, foreign_key: true
 
       t.timestamps
