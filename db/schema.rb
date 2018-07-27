0ab3bade2d1c944d91fa5fed0f00be3e43c10191
Judson Stevens <Judson.Stevens@gmail.com>
2018-07-27 11:13:37 -0600

    Working on eb instance

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_26_224835) do
+ActiveRecord::Schema.define(version: 2018_07_27_170919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,7 +82,7 @@ ActiveRecord::Schema.define(version: 2018_07_26_224835) do
     t.bigint "license_area_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
+    t.integer "user_id"
     t.index ["license_area_id"], name: "index_user_license_areas_on_license_area_id"
   end
 
