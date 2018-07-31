adeba184a369a8a79697234792588657850ff2d8
Judson Stevens <Judson.Stevens@gmail.com>
2018-07-31 16:33:37 -0600

    Implements routes for current user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,13 @@
 class SessionsController < Devise::SessionsController
+  before_action :authenticate_user!, execpt: [:create]
   respond_to :json
 
-  private
-
-  def respond_with(resource, _opts = {})
-    render json: resource
+  def show
+    require 'pry'; binding.pry
+    render json: current_user
   end
+
+  private
 
   def respond_to_on_destroy
     head :no_content
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  
+
   get '/', to: 'homepage#index'
   devise_for :users, defaults: { format: :json }, controllers: { sessions: 'sessions', registrations: 'registrations' }
   namespace :api do
     # post '/register_user', to: 'users#create'
+    get '/current_user', to: 'users#show'
     resources :users, only: [:index]
     get '/user/:id', to: 'users#show'
   end
